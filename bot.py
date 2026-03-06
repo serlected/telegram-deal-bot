1a93c495ed6bf3f309d04ac045eb5a5f148d0786
serlected <ser.hannan@gmail.com>
2026-03-06 22:38:36 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,85 @@
+
+
+
+import asyncio
+import feedparser
+import re
+from telegram import Bot
+
+TOKEN = "8221472099:AAGoB-z88dOYL9hhtfRWV_aOCAiayLofuok"
+CHAT_ID = "@billiger_gehts_nicht"
+
+RSS_URL = "https://www.mydealz.de/rss/deals"
+
+MIN_TEMP = 200
+
+posted_links = set()
+
+async def main():
+    bot = Bot(token=TOKEN)
+
+    print("Bot gestartet...")
+
+    while True:
+        try:
+            feed = feedparser.parse(RSS_URL)
+
+            for entry in feed.entries:
+
+                if entry.link in posted_links:
+                    continue
+
+                title = entry.title
+
+                temp_match = re.search(r'(\d+)°', title)
+
+                if temp_match:
+                    temperature = int(temp_match.group(1))
+                else:
+                    temperature = 0
+
+                if temperature < MIN_TEMP:
+                    continue
+
+                posted_links.add(entry.link)
+
+                link = entry.link
+
+                image = None
+                if "media_content" in entry:
+                    image = entry.media_content[0]["url"]
+
+                message = f"""
+🔥 HOT DEAL ({temperature}°)
+
+{title}
+
+👉 {link}
+"""
+
+                try:
+                    if image:
+                        await bot.send_photo(
+                            chat_id=CHAT_ID,
+                            photo=image,
+                            caption=message
+                        )
+                    else:
+                        await bot.send_message(
+                            chat_id=CHAT_ID,
+                            text=message
+                        )
+
+                    print("HOT Deal gepostet:", title)
+
+                    await asyncio.sleep(3)
+
+                except Exception as e:
+                    print("Telegram Fehler:", e)
+
+        except Exception as e:
+            print("Feed Fehler:", e)
+
+        await asyncio.sleep(60)
+
+asyncio.run(main())
